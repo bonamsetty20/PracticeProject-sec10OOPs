dabb49f77b68bff698cf1846d185feb065fcb01f
bonamsetty20 <bonamsettyjahnavi@gmail.com>
2023-08-02 17:38:36 +0530

    first commit

---
+++ Teacher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProjectSection10_OOPs
+{
+    public class Teacher
+    {
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public string Section { get; set; }
+    }
+
+}
